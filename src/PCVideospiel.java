d22e37db06374fe1dd2a52fb0b01d6400e9ca859
Weisz <naf15@web.de>
2017-05-01 17:45:44 +0200

    Commit 6 All

---
+++ src/PCVideospiel.java
@@ -0,0 +1,23 @@
+
+public class PCVideospiel extends AbstractVideospiel {
+
+	public PCVideospiel(String titel, String kommentar, String system) {
+		super(titel, kommentar, system);
+	}
+
+	@Override
+	protected int getPreisNachTagen(int tage) {
+		if(tage>7){
+			tage-=7;
+			int i=0;
+			while(tage>0){
+				i++;
+				tage-=5;
+			}
+			return 500*i;
+		}
+		return 0;
+	}
+	
+
+}
